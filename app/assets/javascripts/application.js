02dce713832023182c38499e68475340d6cdf26b
teriyaki340600 <tvyq1022@gmail.com>
2020-05-28 11:07:01 +0000

    [finish]texting_application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,51 +31,49 @@ $(function() {
   });
 
 //　select box(camera)
-
-//
-  // return (function () {
-  //   var replaceChildrenOptions, replaceSelectOptions;
-  //   replaceSelectOptions = function ($select, results) {
-  //     $select.html($('<option>'));
-  //     $select.children().remove();
-  //     return $.each(results, function () {
-  //       var option;
-  //       option = $('<option>').val(this.id).text(this.name);
-  //       return $select.append(option);
-  //     });
-  //   };
-  //   replaceChildrenOptions = function () {
-  //     var $selectChildren;
-  //     $selectChildren = $('#photo_camera_id');
-  //     const element = document.getElementById('photo_camera_maker_id');
-  //     const index = element.selectedIndex;
-  //     // 何も選択されていない場合は早期リターンする
-  //     if (index === 0) {
-  //       return $selectChildren.children().remove();
-  //     };
-  //     const childrenPath = element.options[index].dataset.childrenPath;
-  //     // replaceSelectOptions が jquery object を要求しているため
-  //     if (childrenPath != null) {
-  //       return $.ajax({
-  //         url: childrenPath,
-  //         dataType: "json",
-  //         success: function (results) {
-  //           return replaceSelectOptions($selectChildren, results);
-  //         },
-  //         error: function (XMLHttpRequest, textStatus, errorThrown) {
-  //           console.error("Error occurred in replaceChildrenOptions");
-  //           console.log("XMLHttpRequest: " + XMLHttpRequest.status);
-  //           console.log("textStatus: " + textStatus);
-  //           return console.log("errorThrown: " + errorThrown);
-  //         }
-  //       });
-  //     } else {
-  //       return replaceSelectOptions($selectChildren, []);
-  //     }
-  //   };
-  //   const element = document.getElementById('photo_camera_maker_id');
-  //   element.addEventListener('change', () => {
-  //     replaceChildrenOptions();
-  //   })
-  // })();
+  return (function () {
+    var replaceChildrenOptions, replaceSelectOptions;
+    replaceSelectOptions = function ($select, results) {
+      $select.html($('<option>'));
+      $select.children().remove();
+      return $.each(results, function () {
+        var option;
+        option = $('<option>').val(this.id).text(this.name);
+        return $select.append(option);
+      });
+    };
+    replaceChildrenOptions = function () {
+      var $selectChildren;
+      $selectChildren = $('#photo_camera_id');
+      const element = document.getElementById('photo_camera_maker_id');
+      const index = element.selectedIndex;
+      // 何も選択されていない場合は早期リターンする
+      if (index === 0) {
+        return $selectChildren.children().remove();
+      };
+      const childrenPath = element.options[index].dataset.childrenPath;
+      // replaceSelectOptions が jquery object を要求しているため
+      if (childrenPath != null) {
+        return $.ajax({
+          url: childrenPath,
+          dataType: "json",
+          success: function (results) {
+            return replaceSelectOptions($selectChildren, results);
+          },
+          error: function (XMLHttpRequest, textStatus, errorThrown) {
+            console.error("Error occurred in replaceChildrenOptions");
+            console.log("XMLHttpRequest: " + XMLHttpRequest.status);
+            console.log("textStatus: " + textStatus);
+            return console.log("errorThrown: " + errorThrown);
+          }
+        });
+      } else {
+        return replaceSelectOptions($selectChildren, []);
+      }
+    };
+    const element = document.getElementById('photo_camera_maker_id');
+    element.addEventListener('change', () => {
+      replaceChildrenOptions();
+    })
+  })();
 });
