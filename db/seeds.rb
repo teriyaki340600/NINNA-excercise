285c46de8c809c6a608a53f5b05eaaeb99e2bf90
teriyaki340600 <tvyq1022@gmail.com>
2020-05-28 10:50:26 +0000

    [test]application.js

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,38 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#CameraMaker.create!([
-#              {:name => "NIKON"},
-#              {:name => "CANON"},
-#              {:name => "SONY"},
-#              {:name => "LEICA"},
-#              {:name => "SIGMA"},
-#              {:name => "TAMRON"},
-#              {:name => "ZEISS"},
-#              {:name => "PENTAX"},
-#              {:name => "PANASONIC"},
-#              {:name => "FUJIFILM"}
-#              ])
-#Camera.create!(name: "EOS 5D",
-#               camera_maker_id: 2,
-#              introduction: "最高の写真体験",
-#              image_id:
-#              File.open("./app/assets/images/EOS 5D.jpg"))
-#Camera.create!(name: "EOS RP",
-#               camera_maker_id: 2,
-#              introduction: "最高の写真性能",
-#              image_id:
-#              File.open("./app/assets/images/EOS RP.jpg"))
-#Camera.create!(name: "Z7",
-#               camera_maker_id: 1,
-#              introduction: "最高画質のミラーレス",
-#              image_id:
-#              File.open("./app/assets/images/Z7.jpg"))
-#Camera.create!(name: "D500",
-#               camera_maker_id: 1,
-#              introduction: "秒間10コマの連射機",
-#              image_id:
-#              File.open("./app/assets/images/D500.jpg"))
+CameraMaker.create!([
+              {:name => "NIKON"},
+              {:name => "CANON"},
+              {:name => "SONY"},
+              {:name => "LEICA"},
+              {:name => "PENTAX"},
+              {:name => "LUMIX"},
+              {:name => "FUJIFILM"}
+              ])
+Camera.create!(name: "EOS 5D",
+               camera_maker_id: 2,
+              introduction: "最高の写真体験",
+              image_id:
+              File.open("./app/assets/images/EOS 5D.jpg"))
+Camera.create!(name: "EOS RP",
+               camera_maker_id: 2,
+              introduction: "最高の写真性能",
+              image_id:
+              File.open("./app/assets/images/EOS RP.jpg"))
+Camera.create!(name: "Z7",
+               camera_maker_id: 1,
+              introduction: "最高画質のミラーレス",
+              image_id:
+              File.open("./app/assets/images/Z7.jpg"))
+Camera.create!(name: "D500",
+               camera_maker_id: 1,
+              introduction: "秒間10コマの連射機",
+              image_id:
+              File.open("./app/assets/images/D500.jpg"))
 MagnifierMaker.create!([
               {:name => "NIKON"},
               {:name => "CANON"},
