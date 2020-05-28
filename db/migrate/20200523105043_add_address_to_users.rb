b75c8948660763d94810dcdc3304e4fbf2e51782
teriyaki340600 <tvyq1022@gmail.com>
2020-05-28 05:27:57 +0000

    [FIX]remove address column from 20200523105043 migration file

--- db/migrate/20200523105043_add_address_to_users.rb
+++ db/migrate/20200523105043_add_address_to_users.rb
@@ -1,6 +1,5 @@
 class AddAddressToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :address, :string
     add_column :users, :post_code, :string
     add_column :users, :phone_number, :string
     add_column :users, :image_id, :string
